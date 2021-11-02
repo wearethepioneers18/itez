3e79c72c54fcc79a01faee15e16d8da81c9520d1
Alison Mukoma <alison.mukoma@trivago.com>
2021-11-02 12:27:23 +0100

    register new module

--- config/settings/base.py
+++ config/settings/base.py
@@ -76,6 +76,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "itez.users.apps.UsersConfig",
+    "itez.beneficiary.apps.BeneficiaryConfig"
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
