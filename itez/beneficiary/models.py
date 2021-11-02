9df2b6f558ca5299d411a315a505d32f515f775b
MusoleP <85552864+MusoleP@users.noreply.github.com>
2021-11-02 16:04:01 +0200

    Add Permissions Model

--- itez/beneficiary/models.py
+++ itez/beneficiary/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models.deletion import PROTECT
+from django.contrib.auth.models import User 
 from django.utils.translation import gettext_lazy as _
 
 class Province(models.Model):
