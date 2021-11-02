5a9aac009740c2a69713f92b1f504580a44db375
Alison Mukoma <alison.mukoma@trivago.com>
2021-11-02 13:01:02 +0100

    Add district to admin

--- itez/beneficiary/admin.py
+++ itez/beneficiary/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from itez.beneficiary.models import Province
+from itez.beneficiary.models import (
+    Province,
+    District,
+)
 
 class ProvinceAdmin(admin.ModelAdmin):
     list_display = ['name', 'created']
 
+class DistrictAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created']
+
 admin.site.register(Province, ProvinceAdmin)
+admin.site.register(District, DistrictAdmin)
--- itez/beneficiary/models.py
+++ itez/beneficiary/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.deletion import PROTECT
 from django.utils.translation import gettext_lazy as _
 
 class Province(models.Model):
@@ -8,11 +9,34 @@ class Province(models.Model):
     name = models.CharField(
         _("Province"),
         max_length=255, 
-        null=True, 
-        blank=True
         )
 
     created = models.DateTimeField(auto_now_add=True)
     
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name = 'Province'
+        verbose_name_plural = 'Provinces'
+        ordering = ['-created']
+    
+
+class District(models.Model):
+    """
+    Define district properties and corresponding methods.
+    """
+    
+    name = models.CharField(
+        _('District'),
+        max_length=255
+    )
+
+    province = models.ForeignKey(
+        Province,
+        on_delete=PROTECT,
+    )
+
+    created = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return self.name
