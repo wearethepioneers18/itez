1fe7b5daf4cba84f69f02d656633d768ec0e7f60
MusoleP <85552864+MusoleP@users.noreply.github.com>
2021-11-02 16:04:01 +0200

    Add Permissions Model

--- itez/beneficiary/models.py
+++ itez/beneficiary/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models.deletion import PROTECT
+from django.contrib.auth.models import User 
 from django.utils.translation import gettext_lazy as _
 
 class Province(models.Model):
@@ -40,3 +41,23 @@ class District(models.Model):
 
     def __str__(self):
         return self.name
+        
+
+class Permission(models.Model):
+    """
+    Implements agent role permission properties and required methods.
+    """
+    name = models.CharField(
+        _('Agent'),
+        max_length=255
+    )
+
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name = 'Permission'
+        verbose_name_plural = 'Permissions'
+        ordering = ['-created']
+
