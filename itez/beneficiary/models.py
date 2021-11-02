bc00a21ecf7ec05c1ed9ebebca3c6421e8892b71
Alison Mukoma <alison.mukoma@trivago.com>
2021-11-02 12:36:05 +0100

    add province model

--- itez/beneficiary/models.py
+++ itez/beneficiary/models.py
@@ -1,2 +1,17 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+
+class Province(models.Model):
+    """
+    Implements province properties and required methods.
+    """
+    name = models.CharField(
+        _("Province"),
+        max_length=255, 
+        null=True, 
+        blank=True
+        )
+
+    created = models.DateTimeField(auto_now_add=True)
+
+    
