a3843a319cca385b090cd823cd9016f186f638b6
MusoleP <85552864+MusoleP@users.noreply.github.com>
2021-11-02 16:46:35 +0200

    Clean unused import User

--- itez/beneficiary/models.py
+++ itez/beneficiary/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from django.db.models.deletion import PROTECT
-from django.contrib.auth.models import User 
+from django.db.models.deletion import PROTECT 
 from django.utils.translation import gettext_lazy as _
 
 class Province(models.Model):
