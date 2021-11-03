61e9dbfcbbf9540fa8f01feacd07252587a119fa
cazterk <zcephas2@gmail.com>
2021-11-03 11:21:23 +0200

    resolve issues on uuid function

--- itez/beneficiary/uuid.py
+++ itez/beneficiary/uuid.py
@@ -1,11 +1,10 @@
 import uuid
 
 
-def generate_id():
+def generate_uuid_and_agent_code():
     """
-     this function generate a 7 digit code for agent
+     This function generate a 7 digit code for agent.
     """
     uuid_one = uuid.uuid4()
-    print(uuid_one)
     agent_code = str(uuid_one)[1:8].upper()
-    return (agent_code)
+    return uuid_one, agent_code
