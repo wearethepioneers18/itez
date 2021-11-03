154fe85944ba5a5f94886e53a5f35be1324ad704
MusoleP <85552864+MusoleP@users.noreply.github.com>
2021-11-03 13:39:44 +0200

    Add documentation for each user role

--- config/settings/roles.py
+++ config/settings/roles.py
@@ -2,14 +2,34 @@ from rolepermissions.roles import AbstractUserRole
 
 
 class SystemAdmin(AbstractUserRole):
+    """
+    Added custom user roles for system admin as follows.
+
+    System Admin can drop tables.
+    
+    System Admin can manage project manager accounts.
+    
+    System Admin can manage field data collected.
+    
+    System Admin can manage reports generated.
+    """
     available_permissions = {
-        'drop_tables': True,
+        'can_drop_tables': True,
         'can_manage_project_managers': True,
         'can_manage_field_data': True,
         'can_manage_reports': True
     }
 
 class ProjectManagement(AbstractUserRole):
+    """
+    Added custom user roles for Project Manager as follows.
+    
+    Project Manager can view content.
+    
+    Project Manager can manage field personnel accounts.
+    
+    Project Manager can manage donor accounts.
+    """
     available_permissions = {
         'can_view_content': True,
         'can_manage_field_personnel': True,
@@ -17,6 +37,17 @@ class ProjectManagement(AbstractUserRole):
     }
 
 class DatabaseAdministrator(AbstractUserRole):
+    """
+    Added custom user roles for Database Administrator as follows.
+    
+    Database Administrator can view content.
+    
+    Database Administrator can manage field reports.
+    
+    Database Administrator can manage reports.
+    
+    Database Administrator can manage api data.
+    """
     available_permissions = {
         'can_view_content': True,
         'can_manage_field_reports': True,
@@ -25,12 +56,28 @@ class DatabaseAdministrator(AbstractUserRole):
     }
 
 class DonorRepresentative(AbstractUserRole):
+    """
+    Added custom user roles for Donor Representative as follows.
+    
+    Donor Representative can view content.
+
+    Donor Representative can manage reports.
+    """
     available_permissions = {
         'can_view_content': True,
         'can_manage_reports': True
     }
 
 class FieldPersonnel(AbstractUserRole):
+    """
+    Added custom user roles for Field Personnel.
+
+    Field Personnel can view permissions.
+
+    Field Personnel can collect data.
+
+    Field Personnel can create field reports. 
+    """
     available_permissions = {
         'can_view_permissions': True,
         'can_collect_data': True,
