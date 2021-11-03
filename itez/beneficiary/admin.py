d889bfbbe40cb99be3f4e6e2cfb4e61803ed28c6
Chriford <66867121+chriford@users.noreply.github.com>
2021-11-03 22:50:27 +0200

    add work detail model (#6)
    * add work detail model
    
    * enclose field texts of Work Detail table
    
    * rename work detail model instance
    
    * change work address field
    
    * change work address field for work service model
    
    * remove unnecessary migration files
    
    * correct syntax for work detail instance

--- itez/beneficiary/admin.py
+++ itez/beneficiary/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from itez.beneficiary.models import (
     Province,
     District,
+    WorkDetail
 )
 
 class ProvinceAdmin(admin.ModelAdmin):
@@ -10,5 +11,14 @@ class ProvinceAdmin(admin.ModelAdmin):
 class DistrictAdmin(admin.ModelAdmin):
     list_display = ['name', 'created']
 
+class WorkDetailAdmin(admin.ModelAdmin):
+    list_display = [
+        'gross_pay',
+        'company',
+        'work_address',
+        'insured',
+    ]
+
 admin.site.register(Province, ProvinceAdmin)
 admin.site.register(District, DistrictAdmin)
+admin.site.register(WorkDetail ,WorkDetailAdmin)
--- itez/beneficiary/models.py
+++ itez/beneficiary/models.py
@@ -41,3 +41,32 @@ class District(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class WorkDetail(models.Model):
+    """
+    Include Work Detail properties. 
+    """
+    gross_pay = models.DecimalField( 
+        _("Monthly Salary"),
+        decimal_places=2,
+        max_digits=1000,
+        null=False
+    )
+    company = models.CharField(
+        _("Company Name"),
+        max_length=200,
+        null=False
+    )
+    insured = models.BooleanField(
+        _("Company Insured"),
+        default = False
+    )
+    work_address = models.TextField(
+        _("Work Address"),
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.company
