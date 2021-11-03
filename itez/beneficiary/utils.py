244a3b7f0ee458deaa9ae7cb842457856f0e2b8b
cazterk <zcephas2@gmail.com>
2021-11-03 12:48:42 +0200

    uuid => utils

--- itez/beneficiary/utils.py
+++ itez/beneficiary/utils.py
@@ -3,7 +3,7 @@ import uuid
 
 def generate_uuid_and_agent_code():
     """
-     This function generate a 7 digit code for agent.
+     This function generate a 7 digit code for agent and uuid.
     """
     uuid_one = uuid.uuid4()
     agent_code = str(uuid_one)[1:8].upper()
