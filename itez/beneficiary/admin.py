7ad8014c0f141acce914a23b34631bdd8a89d6e3
jmnda-dev <65728679+jmnda-dev@users.noreply.github.com>
2021-11-05 15:26:33 +0200

    Add Beneficiary model (#8)
    * Add beneficiary model
    
    * Register model
    
    * align code top to bottom
    
    * seperate imports
    
    * Reformat code
    
    * format code top to bottom
    
    * Update imports
    
    * Update imports
    
    * Make requested changes
    
    * Add location field and make migrations
    
    * Update location field
    
    * Make migrations and register model in admin
    
    * Update Beneficiary model
    
    * Update Beneficiary model
    
    * Add more fields and make migrations
    
    * Fix typo

--- itez/beneficiary/admin.py
+++ itez/beneficiary/admin.py
@@ -1,29 +1,53 @@
 from django.contrib import admin
+from django.contrib.gis.admin import OSMGeoAdmin
 from itez.beneficiary.models import (
     Province,
     District,
     ServiceArea,
-    WorkDetail
+    WorkDetail,
+    Beneficiary,
+    BeneficiaryParent
 )
 
+
+class BeneficiaryAdmin(OSMGeoAdmin):
+    list_display = [
+        "first_name",
+        "last_name",
+        "beneficiary_id",
+        "created",
+        "location",
+        "parent_details"
+    ]
+
+
+class BeneficiaryParentAdmin(admin.ModelAdmin):
+    list_display = ["father_first_name", "father_last_name", "mother_first_name", "mother_last_name"]
+
 class ProvinceAdmin(admin.ModelAdmin):
-    list_display = ['name', 'created']
+    list_display = ["name", "created"]
+
 
 class DistrictAdmin(admin.ModelAdmin):
-    list_display = ['name', 'created']
+    list_display = ["name", "created"]
+
 
 class ServiceAreaAdmin(admin.ModelAdmin):
-    list_display = ['name', 'created']
+    list_display = ["name", "created"]
+
 
 class WorkDetailAdmin(admin.ModelAdmin):
     list_display = [
-        'gross_pay',
-        'company',
-        'work_address',
-        'insured',
+        "gross_pay",
+        "company",
+        "work_address",
+        "insured",
     ]
+
 
 admin.site.register(Province, ProvinceAdmin)
 admin.site.register(District, DistrictAdmin)
 admin.site.register(ServiceArea, ServiceAreaAdmin)
-admin.site.register(WorkDetail ,WorkDetailAdmin)
+admin.site.register(WorkDetail, WorkDetailAdmin)
+admin.site.register(Beneficiary, BeneficiaryAdmin)
+admin.site.register(BeneficiaryParent, BeneficiaryParentAdmin)
