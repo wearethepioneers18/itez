8dc0ca62a6b345454a90d04dbcef35b66b4afb4e
cazterk <zcephas2@gmail.com>
2021-11-05 10:40:02 +0200

    corrections made assertEqual brought in

--- itez/beneficiary/tests/test_utils.py
+++ itez/beneficiary/tests/test_utils.py
@@ -6,19 +6,34 @@ from itez.beneficiary.utils import generate_uuid_and_agent_code
 class UUIDAndAgentCode(TestCase):
     @classmethod
     def setUp(self):
+        """
+         This method is called for every test that runs and helps with variable declaration
+        """
         self.uuid_code = generate_uuid_and_agent_code()[0]
         self.agent_code = generate_uuid_and_agent_code()[1]
 
         return super().setUp(self)
 
+    def tearDown(self):
+        """
+         This method is called for every test as well
+        """
+        return super().tearDown()
+
     def test_uuid_valid(self):
-        assert len(str(self.uuid_code)) == 36
+        """
+         This function tests for the validity of generated uuid.
+        """
+        self.assertEqual(len(str(self.uuid_code)), 36)
 
     def test_agent_code_length(self):
-        assert len(str(self.agent_code)) == 7
+        """
+         This function tests for length of agent code.
+        """
+        self.assertEqual(len(str(self.agent_code)), 7)
 
     def test_agent_code_upper(self):
-        assert self.agent_code == self.agent_code.upper()
-
-    def tearDown(self) -> None:
-        return super().tearDown()
+        """
+         This function tests for capitalization.
+        """
+        self.assertEqual((self.agent_code), self.agent_code.upper())
