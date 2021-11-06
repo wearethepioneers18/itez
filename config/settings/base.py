2835dab5e8396cb108cad8acc03a9dafa4c46f97
Alison Mukoma <alison.mukoma@trivago.com>
2021-11-06 15:19:35 +0100

    Quickfix #typo

--- config/settings/base.py
+++ config/settings/base.py
@@ -316,7 +316,7 @@ STATICFILES_FINDERS += ["compressor.finders.CompressorFinder"]
 # django-rest-framework - https://www.django-rest-framework.org/api-guide/settings/
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        'rest_framework_simplejwt.authentication.JWTAuthentication'
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
