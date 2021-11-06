2d426b06022787f93044171df67c875460120928
Alison Mukoma <alison.mukoma@trivago.com>
2021-11-06 15:17:03 +0100

    Taskless enforece jwt (#17)
    * update requirements
    
    * Add API docs
    
    * Update .gitignore

--- config/settings/base.py
+++ config/settings/base.py
@@ -316,8 +316,6 @@ STATICFILES_FINDERS += ["compressor.finders.CompressorFinder"]
 # django-rest-framework - https://www.django-rest-framework.org/api-guide/settings/
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.SessionAuthentication",
-        "rest_framework.authentication.TokenAuthentication",
         'rest_framework_simplejwt.authentication.JWTAuthentication'
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
