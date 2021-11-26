4f7626f3ee00e0c90cc6b77ff8ff8c3689d0ffdb
Alison Mukoma <alison.mukoma@trivago.com>
2021-11-26 11:53:01 +0100

    remove  regular  auth

--- config/settings/base.py
+++ config/settings/base.py
@@ -325,9 +325,6 @@ REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": (
         "rest_framework.renderers.JSONRenderer",
         ),
-    "DEFAULT_PERMISSION_CLASSES": (
-        "rest_framework.permissions.IsAuthenticated",
-        ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 10
