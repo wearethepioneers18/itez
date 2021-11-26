8a7aca4a874519dc4b50cafd55415347c6f45d2d
Alison Mukoma <alison.mukoma@trivago.com>
2021-11-26 10:02:45 +0100

    remove email mandate

--- itez/users/models.py
+++ itez/users/models.py
@@ -42,7 +42,7 @@ class UserManager(BaseUserManager):
         if username is None:
             raise TypeError("Superusers must have a unique username.")
 
-        user = self.create_user(username, email, password)
+        user = self.create_user(username, password)
         user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
