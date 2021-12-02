c687224f9729d7daf8fb2e104a0855f8a255694b
wearethepioneers18 <creativedirectorctl2018@gmail.com>
2021-12-02 16:30:05 +0200

    Format beneficiary code

--- itez/beneficiary/admin.py
+++ itez/beneficiary/admin.py
@@ -16,83 +16,79 @@ from itez.beneficiary.models import (
     ImplementingPartner,
     Service,
     ServiceProviderPersonel,
-    ServiceProviderPersonelQualification
+    ServiceProviderPersonelQualification,
 )
 
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 
+
 class LabResource(resources.ModelResource):
     class Meta:
         model = Lab
+
 
 @admin.register(Lab)
 class LabAdmin(ImportExportModelAdmin):
     resource_class = LabResource
 
-    list_display = (
-        'title', 
-        'requested_date'
-        )
+    list_display = ("title", "requested_date")
 
     search_fields = (
-        'title', 
-        'facility__name', 
-        'beneficiary__beneficiary_id', 
-        )
+        "title",
+        "facility__name",
+        "beneficiary__beneficiary_id",
+    )
+
 
 class DrugResource(resources.ModelResource):
     class Meta:
         model = Drug
 
+
 @admin.register(Drug)
 class DrugAdmin(ImportExportModelAdmin):
     resource_class = DrugResource
 
-    list_display = (
-        'name', 
-        'manufacturer', 
-        'expiry_date'
-        )
+    list_display = ("name", "manufacturer", "expiry_date")
 
     search_fields = (
-        'name', 
-        'beneficiary__beneficiary_id', 
-        )
+        "name",
+        "beneficiary__beneficiary_id",
+    )
+
 
 class PrescriptionResource(resources.ModelResource):
     class Meta:
         model = Prescription
 
+
 @admin.register(Prescription)
 class PrescriptionAdmin(ImportExportModelAdmin):
     resource_class = PrescriptionResource
-    list_display = (
-        'title', 
-        'date'
-        )
+    list_display = ("title", "date")
 
-    search_fields = (
-        'title', 
-        )
+    search_fields = ("title",)
+
 
 class ServiceResource(resources.ModelResource):
     class Meta:
         model = Service
+
 
 @admin.register(Service)
 class ServiceAdmin(ImportExportModelAdmin):
     resource_class = ServiceResource
 
     list_display = (
-        'title', 
-        'client_type', 
-        'service_type', 
-        'datetime',
-        )
+        "title",
+        "client_type",
+        "service_type",
+        "datetime",
+    )
+
 
 class FacilityResource(resources.ModelResource):
-
     class Meta:
         model = Facility
 
@@ -100,6 +96,7 @@ class FacilityResource(resources.ModelResource):
 class BeneficiaryResource(resources.ModelResource):
     class Meta:
         model = Beneficiary
+
 
 @admin.register(Beneficiary)
 class BeneficiaryAdmin(ImportExportModelAdmin):
@@ -109,7 +106,7 @@ class BeneficiaryAdmin(ImportExportModelAdmin):
         "beneficiary_id",
         "first_name",
         "last_name",
-        "gender", 
+        "gender",
         "sex",
         "art_status",
         "hiv_status",
@@ -130,17 +127,20 @@ class BeneficiaryAdmin(ImportExportModelAdmin):
         "last_name",
     ]
 
+
 class BeneficiaryResource(resources.ModelResource):
     class Meta:
         model = Beneficiary
+
 
 class BeneficiaryParentAdmin(admin.ModelAdmin):
     list_display = [
         "father_first_name",
         "father_last_name",
-        "mother_first_name", 
-        "mother_last_name"
+        "mother_first_name",
+        "mother_last_name",
     ]
+
 
 class ProvinceAdmin(admin.ModelAdmin):
     list_display = ["name", "created"]
@@ -165,63 +165,65 @@ class WorkDetailAdmin(admin.ModelAdmin):
 
 class AgentDetailAdmin(admin.ModelAdmin):
     list_display = [
-        'first_name',
-        'last_name',
-        'birthdate',
-        'agent_id',
-        'gender',
-        'location'
+        "first_name",
+        "last_name",
+        "birthdate",
+        "agent_id",
+        "gender",
+        "location",
     ]
 
 
 class FacilityTypeAdmin(admin.ModelAdmin):
-    list_display = [
-        'name'
-    ]
+    list_display = ["name"]
+
 
 @admin.register(Facility)
 class FacilityAdmin(ImportExportModelAdmin):
     resource_class = FacilityResource
-    list_display = ('name', 'province', 'district', 'facility_type', 'hmis_code')
+    list_display = ("name", "province", "district", "facility_type", "hmis_code")
 
-    list_display_links = ('name',)
-    search_fields = ('name', 'hmis_code')
+    list_display_links = ("name",)
+    search_fields = ("name", "hmis_code")
 
-    list_filter = ('province', 'facility_type', 'district',)
+    list_filter = (
+        "province",
+        "facility_type",
+        "district",
+    )
     list_per_page = 30
 
 
 class ImplementingPartnerAdmin(admin.ModelAdmin):
-    list_display = [
-        'name',
-        'ip_type'
-    ]
+    list_display = ["name", "ip_type"]
+
+
 class ServiceProviderAdmin(admin.ModelAdmin):
     list_display = [
-        'first_name',
-        'last_name',
-        'qualification',
-        'department',
+        "first_name",
+        "last_name",
+        "qualification",
+        "department",
     ]
 
+
 class ServiceProviderPersonelQualificationAdmin(admin.ModelAdmin):
-    list_display = [
-        'name'
-    ]
+    list_display = ["name"]
+
 
 # register MedicalRecord class
 @admin.register(MedicalRecord)
 class MedicalRecordAdmin(admin.ModelAdmin):
     list_display = [
-        'beneficiary',
-        'service',
-        'prescription',
-        'lab',
+        "beneficiary",
+        "service",
+        "prescription",
+        "lab",
     ]
 
     list_filter = [
-        'beneficiary',
-        'service',
+        "beneficiary",
+        "service",
     ]
     list_per_page = 30
 
@@ -235,4 +237,6 @@ admin.site.register(BeneficiaryParent, BeneficiaryParentAdmin)
 admin.site.register(FacilityType, FacilityTypeAdmin)
 admin.site.register(ImplementingPartner, ImplementingPartnerAdmin)
 admin.site.register(ServiceProviderPersonel, ServiceProviderAdmin)
-admin.site.register(ServiceProviderPersonelQualification, ServiceProviderPersonelQualificationAdmin)
+admin.site.register(
+    ServiceProviderPersonelQualification, ServiceProviderPersonelQualificationAdmin
+)
--- itez/beneficiary/forms.py
+++ itez/beneficiary/forms.py
@@ -9,12 +9,11 @@ from crispy_forms.bootstrap import TabHolder, Tab
 from itez.beneficiary.models import Beneficiary, MedicalRecord
 
 
-
-class MedicalRecordForm(ModelForm):   
+class MedicalRecordForm(ModelForm):
     class Meta:
 
         model = MedicalRecord
-        exclude = ['created']
+        exclude = ["created"]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -22,48 +21,49 @@ class MedicalRecordForm(ModelForm):
         self.helper.form_tag = False
         self.helper.layout = Layout(
             Fieldset(
-                'Service',
+                "Service",
                 Row(
-                    Column('beneficiary', css_class='form-group col-md-6 mb-0'),
-                    Column('service', css_class='form-group col-md-6 mb-0'),
-                    Column('provider_comments', css_class='form-group col-md-6 mb-0'),
-                    Column('interaction_date', css_class='form-group col-md-6 mb-0'),
-                    css_class='form-row'
+                    Column("beneficiary", css_class="form-group col-md-6 mb-0"),
+                    Column("service", css_class="form-group col-md-6 mb-0"),
+                    Column("provider_comments", css_class="form-group col-md-6 mb-0"),
+                    Column("interaction_date", css_class="form-group col-md-6 mb-0"),
+                    css_class="form-row",
                 ),
             ),
             Fieldset(
-                'Medication',
+                "Medication",
                 Row(
-                    Column('prescription', css_class='form-group col-md-4 mb-0'),
-                    Column('no_of_days', css_class='form-group col-md-4 mb-0'),
-                    Column('when_to_take', css_class='form-group col-md-4 mb-0'),
-                    css_class='form-row'
+                    Column("prescription", css_class="form-group col-md-4 mb-0"),
+                    Column("no_of_days", css_class="form-group col-md-4 mb-0"),
+                    Column("when_to_take", css_class="form-group col-md-4 mb-0"),
+                    css_class="form-row",
                 ),
             ),
             Fieldset(
-                'Lab',
+                "Lab",
                 Row(
-                    Column('lab', css_class='form-group col-md-12  mb-0'),
-                    css_class='form-row'
+                    Column("lab", css_class="form-group col-md-12  mb-0"),
+                    css_class="form-row",
                 ),
             ),
             FormActions(
-                Submit('save', 'Create Medica Record'),
+                Submit("save", "Create Medica Record"),
                 HTML('<a class="btn btn-danger" href="/beneficiary/list">Cancel</a>'),
-            )
+            ),
         )
 
     def save(self, commit=True):
         instance = super(BeneficiaryForm, self).save(commit=False)
         instance.save()
-        self.save_m2m()  
+        self.save_m2m()
         return instance
 
-class BeneficiaryForm(ModelForm):   
+
+class BeneficiaryForm(ModelForm):
     class Meta:
 
         model = Beneficiary
-        exclude = ['created', 'beneficiary_id']
+        exclude = ["created", "beneficiary_id"]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -71,49 +71,49 @@ class BeneficiaryForm(ModelForm):
         self.helper.form_tag = False
         self.helper.layout = Layout(
             Fieldset(
-                'Personal Information',
+                "Personal Information",
                 Row(
-                    Column('first_name', css_class='form-group col-md-6 mb-0'),
-                    Column('last_name', css_class='form-group col-md-6 mb-0'),
-                    Column('other_name', css_class='form-group col-md-6 mb-0'),
-                    Column('gender', css_class='form-group col-md-6 mb-0'),
-                    Column('sex', css_class='form-group col-md-6 mb-0'),
-                    Column('profile_photo', css_class='form-group col-md-6 mb-0'),
-                    Column('phone_number', css_class='form-group col-md-6 mb-0'),
-                    Column('email', css_class='form-group col-md-6 mb-0'),
-                    Column('date_of_birth', css_class='form-group col-md-6 mb-0'),
-                    Column('marital_status', css_class='form-group col-md-6 mb-0'),
-                    css_class='form-row'
+                    Column("first_name", css_class="form-group col-md-6 mb-0"),
+                    Column("last_name", css_class="form-group col-md-6 mb-0"),
+                    Column("other_name", css_class="form-group col-md-6 mb-0"),
+                    Column("gender", css_class="form-group col-md-6 mb-0"),
+                    Column("sex", css_class="form-group col-md-6 mb-0"),
+                    Column("profile_photo", css_class="form-group col-md-6 mb-0"),
+                    Column("phone_number", css_class="form-group col-md-6 mb-0"),
+                    Column("email", css_class="form-group col-md-6 mb-0"),
+                    Column("date_of_birth", css_class="form-group col-md-6 mb-0"),
+                    Column("marital_status", css_class="form-group col-md-6 mb-0"),
+                    css_class="form-row",
                 ),
             ),
             Fieldset(
-                'Family Information',
+                "Family Information",
                 Row(
-                    Column('name_of_spouse', css_class='form-group col-md-4 mb-0'),
-                    Column('number_of_children', css_class='form-group col-md-4 mb-0'),
-                    Column('number_of_siblings', css_class='form-group col-md-4 mb-0'),
-                    Column('parent_details', css_class='form-group col-md-4 mb-0'),
-                    css_class='form-row'
+                    Column("name_of_spouse", css_class="form-group col-md-4 mb-0"),
+                    Column("number_of_children", css_class="form-group col-md-4 mb-0"),
+                    Column("number_of_siblings", css_class="form-group col-md-4 mb-0"),
+                    Column("parent_details", css_class="form-group col-md-4 mb-0"),
+                    css_class="form-row",
                 ),
             ),
             Fieldset(
-                'Other Meta Data',
+                "Other Meta Data",
                 Row(
-                    Column('registered_facility', css_class='form-group col-md-4 mb-0'),
-                    Column('service_facility', css_class='form-group col-md-4 mb-0'),
-                    Column('alive', css_class='form-group col-md-4 mb-0'),
-                    Column('education_level', css_class='form-group col-md-4 mb-0'),
-                    Column('hiv_status', css_class='form-group col-md-4 mb-0'),
-                    Column('art_status', css_class='form-group col-md-4 mb-0'),
-                    Column('last_vl', css_class='form-group col-md-4 mb-0'),
-                    Column('alive', css_class='form-group col-md-4 mb-0'),
-                    css_class='form-row'
+                    Column("registered_facility", css_class="form-group col-md-4 mb-0"),
+                    Column("service_facility", css_class="form-group col-md-4 mb-0"),
+                    Column("alive", css_class="form-group col-md-4 mb-0"),
+                    Column("education_level", css_class="form-group col-md-4 mb-0"),
+                    Column("hiv_status", css_class="form-group col-md-4 mb-0"),
+                    Column("art_status", css_class="form-group col-md-4 mb-0"),
+                    Column("last_vl", css_class="form-group col-md-4 mb-0"),
+                    Column("alive", css_class="form-group col-md-4 mb-0"),
+                    css_class="form-row",
                 ),
             ),
             FormActions(
-                Submit('save', 'Create Beneficiary'),
+                Submit("save", "Create Beneficiary"),
                 HTML('<a class="btn btn-danger" href="/beneficiary/list">Cancel</a>'),
-            )
+            ),
         )
 
     def save(self, commit=True):
@@ -121,4 +121,3 @@ class BeneficiaryForm(ModelForm):
         instance.save()
         # self.save_m2m()  # we  can use this if we have many to many field on the model i.e Service
         return instance
-       
